e2362439790170e6c5a27f27bcb69d01a0718ba2
Matheus Silva <msilvaqs@outlook.com>
2024-09-05 22:23:32 -0400

    Initial graphql yoga server

---
+++ src/schema.ts
@@ -0,0 +1,17 @@
+import { createSchema } from "graphql-yoga";
+
+const typeDefinitions = /* GraphQL */ `
+  type Query {
+    hello: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello World!",
+  },
+};
+export const schema = createSchema({
+  resolvers: [resolvers],
+  typeDefs: [typeDefinitions],
+});
